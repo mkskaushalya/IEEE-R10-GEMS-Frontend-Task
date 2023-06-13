5fa4cafc52db408b73c39fe3dd8056356f83a62b
Sahan Kaushalya <mkskaushalya@gmail.com>
2023-06-14 00:37:10 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,40 @@ hamb.addEventListener("click", () => {
     hamb.classList.toggle("active");
     menubar.classList.toggle("active");
 });
+
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+
+const navlinks = document.querySelectorAll(".navlink");
+navlinks.forEach(navlink => {
+    navlink.addEventListener("click", () => {
+        // document.querySelector(".navlink.active")
+        if(navlink.classList.contains("active")){
+            document.querySelector('.navlink.active')?.classList.remove('active');
+            // navlink.classList.remove('active')
+        }else{
+
+            document.querySelector('.navlink.active')?.classList.remove('active');
+            navlink.classList.add("active");
+        }
+    })
+    // navlink.addEventListener("mouseover", () => {
+    //     // document.querySelector(".navlink.active")
+    //     if(navlink.classList.contains("active")){
+    //         document.querySelector('.active')?.classList.remove('active');
+    //         // navlink.classList.remove('active')
+    //     }else{
+
+    //         document.querySelector('.active')?.classList.remove('active');
+    //         navlink.classList.add("active");
+    //     }
+    // })
+    // navlink.addEventListener("mouseleave", () => {
+    //     document.querySelector('.active')?.classList.remove('active');
+    // })
+})
